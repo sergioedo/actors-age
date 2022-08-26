73caac726f1a0db4bff822e09f113a3c408cadd9
sergioedo <sergio.edo@gmail.com>
2022-08-26 09:38:18 +0200

    fix some html props

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -94,30 +94,30 @@ export default function Index() {
                 search it.
               </p>
               <div className="mx-auto mt-10 max-w-sm sm:max-w-none sm:justify-center">
-                <form class="flex items-center">
-                  <label for="simple-search" class="sr-only">
+                <form className="flex items-center">
+                  <label htmlFor="simple-search" className="sr-only">
                     Search
                   </label>
-                  <div class="relative w-full">
-                    <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                  <div className="relative w-full">
+                    <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
                       <svg
                         aria-hidden="true"
-                        class="h-5 w-5 text-gray-500 dark:text-gray-400"
+                        className="h-5 w-5 text-gray-500 dark:text-gray-400"
                         fill="currentColor"
                         viewBox="0 0 20 20"
                         xmlns="http://www.w3.org/2000/svg"
                       >
                         <path
-                          fill-rule="evenodd"
+                          fillRule="evenodd"
                           d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                          clip-rule="evenodd"
+                          clipRule="evenodd"
                         ></path>
                       </svg>
                     </div>
                     <input
                       type="text"
                       id="simple-search"
-                      class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 pl-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500  dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                      className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 pl-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500  dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
                       placeholder="Search for an actor, actress, movie, serie..."
                       required
                     />
