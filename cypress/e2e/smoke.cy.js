bcc8d9761d1a4d2e0c1758dd6e0ff4a7b3b5112b
sergioedo <sergio.edo@gmail.com>
2022-08-27 08:41:26 +0200

    fix cypress - element not mounted

--- cypress/e2e/smoke.cy.js
+++ cypress/e2e/smoke.cy.js
@@ -1,6 +1,6 @@
 describe("smoke tests", () => {
   it("should allow you to write text on the search bar", () => {
     cy.visitAndCheck("/");
-    cy.get("#simple-search").type("this is a test");
+    cy.get("#simple-search").type("this is a test", { force: true });
   });
 });
