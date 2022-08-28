b60ac8d3d554707d58c6b9194c4c123899100bd5
sergioedo <sergio.edo@gmail.com>
2022-08-28 17:57:03 +0200

    floor fade background image

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -51,8 +51,8 @@ export default function Index() {
     <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
       <div className="relative sm:pb-16 sm:pt-8">
         <div className="max-w-9xl mx-auto sm:px-6 lg:px-8">
-          <div className="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
-            <div className="absolute inset-0">
+          <div className="relative bg-black shadow-xl sm:overflow-hidden sm:rounded-2xl">
+            <div className="absolute">
               <img
                 className="h-full w-full object-cover"
                 src="/img/jason-dent-SnXIF8_2oPw-unsplash.jpeg"
@@ -60,6 +60,7 @@ export default function Index() {
               />
 
               <div className="absolute inset-0 bg-[color:rgba(254,204,27,0.5)] mix-blend-multiply" />
+              <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black" />
             </div>
             <div className="relative px-4 pt-16 pb-8 sm:px-6 sm:pt-24 sm:pb-14 lg:px-8 lg:pb-20 lg:pt-32">
               <h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
