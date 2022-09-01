a50071e29c310dafef0e9e33898516783baf7848
sergioedo <sergio.edo@gmail.com>
2022-09-01 21:15:59 +0200

    refactor Search Form

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { json } from "@remix-run/node";
 import {
   useLoaderData,
-  Form,
   useSearchParams,
   useTransition,
 } from "@remix-run/react";
@@ -11,6 +10,7 @@ import MadeWith from "~/components/MadeWith";
 import PersonMediaCard from "~/components/PersonMediaCard";
 import MovieMediaCard from "~/components/MovieMediaCard";
 import Loading from "~/components/Loading";
+import SearchBar from "~/components/SearchBar";
 
 const fetchPersonDetails = async (media) => {
   const { id } = media;
@@ -121,38 +121,7 @@ export default function Index() {
                 search it.
               </p>
               <div className="mx-auto mt-10 max-w-sm sm:max-w-none sm:justify-center">
-                <Form className="flex items-center" action=".">
-                  <label htmlFor="simple-search" className="sr-only">
-                    Search
-                  </label>
-                  <div className="relative w-full">
-                    <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                      <svg
-                        aria-hidden="true"
-                        className="h-5 w-5 text-gray-500 dark:text-gray-400"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                          clipRule="evenodd"
-                        ></path>
-                      </svg>
-                    </div>
-                    <input
-                      type="text"
-                      id="simple-search"
-                      name="query"
-                      className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 pl-10 text-sm text-gray-900 focus:border-blue-500 focus:bg-gray-100 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-800 dark:focus:bg-gray-700 dark:focus:ring-blue-500"
-                      placeholder="Search for actors, movies, tv shows..."
-                      defaultValue={query}
-                      onKeyUp={handleKeyUp}
-                      required
-                    />
-                  </div>
-                </Form>
+                <SearchBar query={query} handleKeyUp={handleKeyUp} />
               </div>
               {transition.state === "idle" ? (
                 results.length ? (
